0980db17af2d1adfcc27cc8438141d470d8da0a4
1blackghost <ashishjosephnew@gmail.com>
2024-11-06 20:56:53 +0530

    intiatives

--- heartify/settings.py
+++ heartify/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(_file_).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     
 ]
 
-ROOT_URLCONF = 'mystery_digits_reloaded.urls'
+ROOT_URLCONF = 'heartify.urls'
 
 TEMPLATES = [
     {
