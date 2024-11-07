e4aac94369454f79ccbdcf65c745b9c4bd211367
1blackghost <ashishjosephnew@gmail.com>
2024-11-07 06:37:50 +0530

    send

--- main/views.py
+++ main/views.py
@@ -92,15 +92,15 @@ def get_results_view(request):
 @csrf_exempt
 def signup_view(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
-        username = data.get('username')
-        password = data.get('password')
-        email = data.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        phone_number = request.POST.get('phone_number')
 
         if User.objects.filter(username=username).exists():
             return JsonResponse({'error': 'Username already exists'}, status=400)
 
-        user = User.objects.create_user(username=username, email=email, password=password)
+        user = User.objects.create_user(username=username, email=email, password=password,phone_number=phone_number)
 
         auth_login(request, user)
         return JsonResponse({'message': 'User registered successfully'}, status=201)
@@ -110,9 +110,8 @@ def signup_view(request):
 @csrf_exempt
 def login_view(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
-        username = data.get('username')
-        password = data.get('password')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
         user = authenticate(request, username=username, password=password)
         
@@ -128,24 +127,22 @@ def login_view(request):
 @login_required
 def save_heart_condition(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
-
         heart_condition_data = {
-            'male': int(data.get('male')),
-            'age': int(data.get('age')),
-            'education': int(data.get('education')),
-            'currentSmoker': int(data.get('currentSmoker')),
-            'cigsPerDay': int(data.get('cigsPerDay')),
-            'BPMeds': int(data.get('BPMeds')),
-            'prevalentStroke': int(data.get('prevalentStroke')),
-            'prevalentHyp': int(data.get('prevalentHyp')),
-            'diabetes': int(data.get('diabetes')),
-            'totChol': int(data.get('totChol')),
-            'sysBP': int(data.get('sysBP')),
-            'diaBP': int(data.get('diaBP')),
-            'BMI': int(data.get('BMI')),
-            'heartRate': int(data.get('heartRate')),
-            'glucose': int(data.get('glucose')),
+            'male': int(request.POST.get('male')),
+            'age': int(request.POST.get('age')),
+            'education': int(request.POST.get('education')),
+            'currentSmoker': int(request.POST.get('currentSmoker')),
+            'cigsPerDay': int(request.POST.get('cigsPerDay')),
+            'BPMeds': int(request.POST.get('BPMeds')),
+            'prevalentStroke': int(request.POST.get('prevalentStroke')),
+            'prevalentHyp': int(request.POST.get('prevalentHyp')),
+            'diabetes': int(request.POST.get('diabetes')),
+            'totChol': int(request.POST.get('totChol')),
+            'sysBP': int(request.POST.get('sysBP')),
+            'diaBP': int(request.POST.get('diaBP')),
+            'BMI': int(request.POST.get('BMI')),
+            'heartRate': int(request.POST.get('heartRate')),
+            'glucose': int(request.POST.get('glucose')),
         }
 
         user = request.user
